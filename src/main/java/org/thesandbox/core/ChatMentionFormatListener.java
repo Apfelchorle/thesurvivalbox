0fb7bbc2046e0df088675e7530ea76831ccf2797
Apfelchorle <apfelchorle@gmail.com>
2026-09-13 17:45:23 +0100

    fixed rank tags
    Signed-off-by: Apfelchorle <apfelchorle@gmail.com>

--- src/main/java/org/thesandbox/core/ChatMentionFormatListener.java
+++ src/main/java/org/thesandbox/core/ChatMentionFormatListener.java
@@ -2,12 +2,10 @@ package org.thesandbox.core;
 
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
-import io.papermc.paper.event.player.AsyncChatEvent;
-import net.luckperms.api.LuckPerms; // optional import if you reference the type elsewhere
-import net.luckperms.api.LuckPermsProvider;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -305,8 +303,7 @@ public class ChatMentionFormatListener implements Listener
             // Fall through to Bukkit display name.
         }
 
-        String displayName = p.getDisplayName();
-        return displayName == null || displayName.isBlank() ? p.getName() : displayName;
+        return p.getName();
     }
 
 
